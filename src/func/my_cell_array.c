8a04cb659725d43b8fe15f85a8fb0095405299c5
FukuInTheCode <tomc.code@gmail.com>
2023-09-11 23:36:41 +0200

    add a cell get array

---
+++ src/func/my_cell_array.c
@@ -0,0 +1,8 @@
+#include "../../includes/my.h"
+
+uint32_t my_cell_get_array(void *cell_ptr, double **arr, void *params)
+{
+    my_cell_t *cell = (my_cell_t *)cell_ptr;
+    my_nn_to_array(&(cell->brain), arr);
+    return my_nn_get_n_params(&(cell->brain));
+}
