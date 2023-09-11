52fa988b3a21fffda475e948cd5f9318354112c0
FukuInTheCode <tomc.code@gmail.com>
2023-09-11 23:33:06 +0200

    add a select cell

--- src/func/my_cell_select.c
+++ src/func/my_cell_select.c
@@ -1,3 +1,11 @@
 #include "../../includes/my.h"
 
-bool my_cell_is_select(void *cell_ptr, void *reward, uint32_t )
+bool my_cell_is_select(void *cell_ptr, void *reward, uint32_t pop_size, void *params)
+{
+    my_cell_t *cell = (my_cell_t *)cell_ptr;
+    double x = cell->atb.arr[0][0];
+    double y = cell->atb.arr[1][0];
+    if (x < SIZE / 3. * 2)
+        return false;
+    return true;
+}
