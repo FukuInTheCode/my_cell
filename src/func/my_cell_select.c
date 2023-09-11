82a75355a3d8b88c672fee5dc833901961d80439
FukuInTheCode <tomc.code@gmail.com>
2023-09-11 23:55:44 +0200

    chanegs

--- src/func/my_cell_select.c
+++ src/func/my_cell_select.c
@@ -5,7 +5,6 @@ bool my_cell_is_select(void *cell_ptr, void *reward,\
 {
     my_cell_t *cell = (my_cell_t *)cell_ptr;
     double x = cell->atb.arr[0][0];
-    double y = cell->atb.arr[1][0];
     if (x < SIZE / 3. * 2)
         return false;
     return true;
