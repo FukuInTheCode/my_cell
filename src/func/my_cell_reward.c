5765f1849293efa879c542033e19c4f44c0c0fd8
FukuInTheCode <tomc.code@gmail.com>
2023-09-11 23:55:32 +0200

    chanegs

--- src/func/my_cell_reward.c
+++ src/func/my_cell_reward.c
@@ -5,6 +5,5 @@ double my_cell_get_reward(void *cell_ptr, void *pop,\
 {
     my_cell_t *cell = (my_cell_t *)cell_ptr;
     double x = cell->atb.arr[0][0];
-    double y = cell->atb.arr[1][0];
     return SIZE - x;
 }
