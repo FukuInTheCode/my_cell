af8f207b43d012a04c7d57560f38605363ea1d48
FukuInTheCode <tomc.code@gmail.com>
2023-09-11 23:30:51 +0200

    bug fix

--- src/func/my_cell_reward.c
+++ src/func/my_cell_reward.c
@@ -1,7 +1,7 @@
 #include "../../includes/my.h"
 
 double my_cell_get_reward(void *cell_ptr, void *pop,\
-                            void *pop_size, void *params)
+                            uint32_t pop_size, void *params)
 {
     my_cell_t *cell = (my_cell_t *)cell_ptr;
     double x = cell->atb.arr[0][0];
