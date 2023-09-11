706360328bb69f0e2d4daa1fb9d6c899e2579cff
FukuInTheCode <tomc.code@gmail.com>
2023-09-11 22:46:05 +0200

    chaneg in maiçn

--- src/main.c
+++ src/main.c
@@ -1,6 +1,30 @@
 #include "../includes/my.h"
 
-
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
+    srand(time(0));
+    my_evo_t evo = {
+        .pop_size = 100,
+        .max_tick_per_gen = SIZE,
+        .mutation_chance = 0.3,
+        .mutation_range = 1.,
+        .agent_struct_size = sizeof(my_cell_t),
+        .gen_skip = 10,
+        .selected_size = 30,
+        .draw_agent = my_cell_draw,
+        .update_agent = my_cell_update,
+        .agent_is_select = my_cell_is_select,
+        .get_agent_reward = my_cell_get_reward,
+        .get_agent_array = my_cell_get_array,
+        .reset_agent = my_cell_reset,
+        .agent_from_array = my_cell_from_array,
+        .create_agent = my_cell_create,
+        .free_agent = my_cell_free,
+        .params = NULL
+    };
+    my_evo_create(&evo);
+    sfVideoMode mode = {1500, 1500, 32};
+    my_evo_viz(&evo, mode);
+    my_evo_free(&evo);
     return 0;
 }
