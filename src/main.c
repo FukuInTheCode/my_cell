38bef20c1bf07bc72f0355580f1e23878aece98d
FukuInTheCode <tomc.code@gmail.com>
2023-09-11 23:37:09 +0200

    chanegs

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@ int main(int argc, char* argv[])
         // .update_agent = my_cell_update,
         .agent_is_select = my_cell_is_select,
         .get_agent_reward = my_cell_get_reward,
-        // .get_agent_array = my_cell_get_array,
+        .get_agent_array = my_cell_get_array,
         // .reset_agent = my_cell_reset,
         // .agent_from_array = my_cell_from_array,
         .create_agent = my_cell_create,
