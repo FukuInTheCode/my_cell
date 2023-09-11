1617fad4bd1639e680301c8a1b516e255447d2fb
FukuInTheCode <tomc.code@gmail.com>
2023-09-11 23:33:35 +0200

    chaneg in the select cell

--- src/func/my_cell_select.c
+++ src/func/my_cell_select.c
@@ -1,6 +1,7 @@
 #include "../../includes/my.h"
 
-bool my_cell_is_select(void *cell_ptr, void *reward, uint32_t pop_size, void *params)
+bool my_cell_is_select(void *cell_ptr, void *reward,\
+                            uint32_t pop_size, void *params)
 {
     my_cell_t *cell = (my_cell_t *)cell_ptr;
     double x = cell->atb.arr[0][0];
