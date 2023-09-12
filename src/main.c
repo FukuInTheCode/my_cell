8d00b75e41f033f7f1788a6428c495bf7cda42e9
FukuInTheCode <tomc.code@gmail.com>
2023-09-12 15:41:18 +0200

    upadtez cell v1 rename

--- src/main.c
+++ src/main.c
@@ -15,7 +15,7 @@ int main(int argc, char* argv[])
         .params = NULL,
         .draw_agent = my_cell_draw,
         .draw_bg = my_cell_draw_bg,
-        .update_agent = my_cell_update,
+        .update_agent = my_cell_update_v1,
         .agent_is_select = my_cell_is_select,
         .get_agent_reward = my_cell_get_reward,
         .get_agent_array = my_cell_get_array,
