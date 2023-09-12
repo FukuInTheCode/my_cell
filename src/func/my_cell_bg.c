9b1e1820a7917393d609ae5d4bbae185d684582c
FukuInTheCode <tomc.code@gmail.com>
2023-09-12 16:03:55 +0200

    change in cell bg

--- src/func/my_cell_bg.c
+++ src/func/my_cell_bg.c
@@ -1,7 +1,15 @@
+#define MATRIX_INIT_STR
 #include "../../includes/my.h"
 
 void my_cell_draw_bg(sfRenderWindow *window, void *params)
 {
+    my_global_params_t *gps = (my_global_params_t *)params;
+    sfText *txt = sfText_create();
+    char *str = init_str("gen n'", gps->gen_i)
+    sfText_setString(txt, str);
+    sfRenderWindow_drawText(window, txt, NULL);
+    free(str);
+
     sfRenderWindow_clear(window, sfBlack);
     sfVector2u window_size = sfRenderWindow_getSize(window);
     sfVector2f ratio = {
