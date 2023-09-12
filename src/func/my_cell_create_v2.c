1c9d1ab156eb1c5001b98df870e2ea99aba2fdf7
FukuInTheCode <tomc.code@gmail.com>
2023-09-12 21:16:45 +0200

    craetae a create func v2

---
+++ src/func/my_cell_create_v2.c
@@ -0,0 +1,19 @@
+#include "../../includes/my.h"
+
+void my_cell_create_v1(void *cell_ptr, void *params)
+{
+    my_cell_t *cell = (my_cell_t *)cell_ptr;
+    uint32_t dims[] = {2, 3, 3, 3};
+    cell->brain.name = "brain";
+    cell->brain.dims = dims;
+    cell->brain.size = 4;
+    cell->brain.acti_type = base_type;
+    cell->brain.funcs.af = my_nn_sin;
+    cell->brain.funcs.grad_af = my_nn_sin_grad;
+    my_nn_create(&(cell->brain));
+    cell->atb.m = 0;
+    cell->atb.n = 0;
+    my_matrix_create(2, 1, 1, &(cell->atb));
+    my_matrix_randint(0, SIZE, 1, &(cell->atb));
+    cell->color = sfRed;
+}
