7082a03d38c8b2b7825149a53e2d553d60161cd5
FukuInTheCode <tomc.code@gmail.com>
2023-09-12 13:55:04 +0200

    change in draw

--- src/func/my_cell_draw.c
+++ src/func/my_cell_draw.c
@@ -5,8 +5,8 @@ void my_cell_draw(sfRenderWindow * window, void *cell_ptr, void *params)
     my_cell_t *cell = (my_cell_t *)cell_ptr;
     sfVector2u window_size = sfRenderWindow_getSize(window);
     sfVector2f ratio = {
-        .x = window_size.x / SIZE,
-        .y = window_size.y / SIZE
+        .x = (window_size.x - RADIUS)/ SIZE,
+        .y = (window_size.y - RADIUS) / SIZE
     };
     sfCircleShape *pt = sfCircleShape_create();
     sfCircleShape_setFillColor(pt, cell->color);
