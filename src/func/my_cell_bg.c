f7b4063435c9275664d13a01de91e530d11934d4
FukuInTheCode <tomc.code@gmail.com>
2023-09-12 16:04:56 +0200

    chaneg in update parasm

--- src/func/my_cell_bg.c
+++ src/func/my_cell_bg.c
@@ -4,9 +4,9 @@
 void my_cell_draw_bg(sfRenderWindow *window, void *params)
 {
     my_global_params_t *gps = (my_global_params_t *)params;
-    sfText *txt = sfText_create();
     char *str = init_str("gen n'", gps->gen_i);
-    sfText_setString(txt, str);
+    sfText *txt = sfText_create();
+    // add the code
     sfRenderWindow_drawText(window, txt, NULL);
     free(str);
 
--- src/func/my_cell_params.c
+++ src/func/my_cell_params.c
@@ -3,6 +3,5 @@
 void my_cell_update_params(void *pop, uint32_t pop_size, void *params_ptr)
 {
     my_global_params_t *gps = (my_global_params_t *)params_ptr;
-    printf("gen n'%u\n", gps->gen_i);
     ++(gps->gen_i);
 }
