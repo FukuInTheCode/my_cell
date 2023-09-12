d30f12557aa5841e1a406e19fac8d923df8db4c5
FukuInTheCode <tomc.code@gmail.com>
2023-09-12 13:32:27 +0200

    bug fix

--- src/func/my_cell_array.c
+++ src/func/my_cell_array.c
@@ -1,10 +1,13 @@
+#define MATRIX_CHECK_ALLOC
 #include "../../includes/my.h"
 
 uint32_t my_cell_get_array(void *cell_ptr, double **arr, void *params)
 {
     my_cell_t *cell = (my_cell_t *)cell_ptr;
+    uint32_t arr_size = my_nn_get_n_params(&(cell->brain));
+    *arr = calloc(arr_size, sizeof(double));
     my_nn_to_array(&(cell->brain), arr);
-    return my_nn_get_n_params(&(cell->brain));
+    return arr_size;
 }
 
 void my_cell_from_array(void *cell_ptr, double *arr, void *params)
