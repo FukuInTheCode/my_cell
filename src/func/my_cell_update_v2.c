fc45c93e693974039024269c621cd1c77fe309dc
FukuInTheCode <tomc.code@gmail.com>
2023-09-13 12:29:20 +0200

    chanegs

--- src/func/my_cell_update_v2.c
+++ src/func/my_cell_update_v2.c
@@ -1,35 +1,6 @@
 #define UPDATE_CELL_NATB_FUNC
 #include "../../includes/my.h"
 
-static double normalize(double x)
-{
-    return (x > 0) - (x < 0);
-}
-
-static double control_coords(double x)
-{
-    if (x < 0)
-        return 0;
-    if (x > SIZE)
-        return SIZE;
-    return x;
-}
-
-
-static void check_new_atb(my_matrix_t *old_atb, uint32_t pop_size,\
-                                        void *pop, my_matrix_t *new_atb)
-{
-    my_matrix_applyfunc_2(new_atb, control_coords);
-    for (uint32_t i = 0; i < pop_size; ++i) {
-        my_cell_t *cell_ptr = (my_cell_t *)((char *)pop +\
-                                        i * sizeof(my_cell_t));
-        if (!my_matrix_equals(&(cell_ptr->atb), new_atb))
-            continue;
-        new_atb->arr[1][0] = old_atb->arr[1][0];
-        new_atb->arr[0][0] = old_atb->arr[0][0];
-    }
-}
-
 uint32_t my_cell_update_v2(void *cell_ptr, void *pop,\
                                 uint32_t pop_size, void *params)
 {
