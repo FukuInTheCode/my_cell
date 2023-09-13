c6f897545d63736330c3a505d7374362bd95a1e1
FukuInTheCode <tomc.code@gmail.com>
2023-09-13 13:12:55 +0200

    chaneg in v2

--- src/func/my_cell_update_v1.c
+++ src/func/my_cell_update_v1.c
@@ -7,7 +7,7 @@ uint32_t my_cell_update_v1(void *cell_ptr, void *pop,\
     my_cell_t *cell = (my_cell_t *)cell_ptr;
     MAT_DECLA(datb);
     my_nn_predict(&(cell->brain), &(cell->atb), &datb);
-    my_matrix_applyfunc_2(&datb, normalize);
+    my_matrix_applyfunc_2(&datb, normalize2);
     MAT_DECLA(new_atb);
     my_matrix_add(&new_atb, 2, &(cell->atb), &datb);
     check_new_atb(&(cell->atb), pop_size, pop, &new_atb);
