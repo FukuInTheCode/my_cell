1cf360c718ba21f49b317b5afd5851f7eae6d6fc
FukuInTheCode <tomc.code@gmail.com>
2023-09-13 12:55:45 +0200

    chnage in arrays func

--- src/func/my_cell_array.c
+++ src/func/my_cell_array.c
@@ -7,11 +7,13 @@ uint32_t my_cell_get_array(void *cell_ptr, double **arr, void *params)
     uint32_t arr_size = my_nn_get_n_params(&(cell->brain));
     *arr = calloc(arr_size, sizeof(double));
     my_nn_to_array(&(cell->brain), arr);
+    cell->color = sfBlue;
     return arr_size;
 }
 
 void my_cell_from_array(void *cell_ptr, double *arr, void *params)
 {
     my_cell_t *cell = (my_cell_t *)cell_ptr;
+    cell->color = sfRed;
     my_nn_from_array(&(cell->brain), arr);
 }
